bd88c16f7f98b09a59108653d039f0141a9b52de
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2012-05-22 22:24:41 +0900

    inlineを付加

--- lua_util/function.hpp
+++ lua_util/function.hpp
@@ -7,8 +7,10 @@
 
 namespace lua_util{
 
-template<int N, class R, class... T>
-class function{
+template<class> class function;
+
+template<class R, class...ArgTypes>
+class function<R(ArgTypes...)>{
 	public:
 		typedef type::number_t number_t;
 		typedef type::string_t string_t;
@@ -18,7 +20,7 @@ class function{
 		function(lua_State* lua, std::string const& func_name);
 		~function();
 	public:
-		R operator () (T const&... args) const;
+		R operator () (ArgTypes const&... args) const;
 	private:
 		template<class...Rest>
 			void push_args(number_t const& arg, Rest const&... rests) const;
@@ -34,19 +36,24 @@ class function{
 		std::string      _func_name;
 };
 
-template<int N, class R, class... T>
-function<N, R, T...>::function(lua_State* lua, std::string const& func_name) : _lua(lua), _func_name(func_name){
+template<class R, class...ArgTypes>
+inline
+function<R(ArgTypes...)>::function(lua_State* lua, std::string const& func_name) : _lua(lua), _func_name(func_name){
 
 }
 
-template<int N, class R, class...T>
-function<N, R, T...>::~function(){
+template<class R, class...ArgTypes>
+inline
+function<R(ArgTypes...)>::~function(){
 	_lua=       nullptr; // 参照を持っているだけなので、deleteしない
 	_func_name= "";
 }
 
-template<int N, class R, class... T>
-R function<N, R, T...>::operator () (T const&... args) const{
+template<class R, class...ArgTypes>
+inline
+R function<R(ArgTypes...)>::operator () (ArgTypes const&... args) const{
+	int constexpr ret_values= 1; // 現状1つの返り値のみに対応
+
 	lua_settop(_lua, 0);
 	
 	lua_getfield(_lua, LUA_GLOBALSINDEX, _func_name.c_str());
@@ -54,7 +61,7 @@ R function<N, R, T...>::operator () (T const&... args) const{
 	push_args(args...);
 
     // sizeof...(args)個の引数とN個の返り値で関数をコール
-    int const err_code= lua_pcall(_lua, sizeof...(args), N, 0);
+    int const err_code= lua_pcall(_lua, sizeof...(args), ret_values, 0);
 
 	switch(err_code){
 		case LUA_ERRRUN:
@@ -69,32 +76,37 @@ R function<N, R, T...>::operator () (T const&... args) const{
 	return 0;
 }
 
-template<int N, class R, class... T>
+template<class R, class...ArgTypes>
 template<class...Rest>
-void function<N, R, T...>::push_args(number_t const& arg, Rest const&... rests) const{
+inline
+void function<R(ArgTypes...)>::push_args(number_t const& arg, Rest const&... rests) const{
 	lua_pushnumber(_lua, arg);
 }
 
-template<int N, class R, class... T>
+template<class R, class...ArgTypes>
 template<class...Rest>
-void function<N, R, T...>::push_args(string_t const& arg, Rest const&... rests) const{
+inline
+void function<R(ArgTypes...)>::push_args(string_t const& arg, Rest const&... rests) const{
 	lua_pushstring(_lua, arg);
 }
 
-template<int N, class R, class... T>
+template<class R, class...ArgTypes>
 template<class...Rest>
-void function<N, R, T...>::push_args(boolean_t const& arg, Rest const&... rests) const{
+inline
+void function<R(ArgTypes...)>::push_args(boolean_t const& arg, Rest const&... rests) const{
 	lua_pushboolean(_lua, arg);
 }
 
-template<int N, class R, class... T>
+template<class R, class...ArgTypes>
 template<class...Rest>
-void function<N, R, T...>::push_args(nil_t const& arg, Rest const&... rests) const{
+inline
+void function<R(ArgTypes...)>::push_args(nil_t const& arg, Rest const&... rests) const{
 	lua_pushnil(_lua);
 }
 
-template<int N, class R, class... T>
-void function<N, R, T...>::push_args() const{}
+template<class R, class...ArgTypes>
+inline
+void function<R(ArgTypes...)>::push_args() const{}
 
 }
 
